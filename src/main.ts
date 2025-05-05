2ef4e49be53978c08089f76c3845e3320c444c4a
Diego Trejo <124468131+trejo903@users.noreply.github.com>
2025-05-04 19:34:45 -0600

    first commit

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,14 @@ import { ValidationPipe } from '@nestjs/common';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 
+const port = process.env.PORT || 3000
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(new ValidationPipe({
     whitelist:true
   }))
   app.useStaticAssets(join(__dirname,'../public'))
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 bootstrap();
